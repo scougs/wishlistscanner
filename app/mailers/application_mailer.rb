95a6168430d8e932609a34bd36f36d83c981291e
Stewart Scougall <Scougs@Stewarts-MacBook-Pro.local>
2015-04-25 15:17:53 +0300

    updated the From address in mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,12 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "info@wishlistscanner.com"
+  default from: "WishlistScanner <info@wishlistscanner.com>"
   layout 'mailer'
 
   def daily_admin_email(scan_id)
     @scan = Scan.find(scan_id)
     @admin_email = ENV['ADMIN_EMAIL']
 
-    mail to: @admin_email, from: "system@wishlistscanner.com", subject: "WishlistScanner: Daily System Admin Email #{Date.today}"
+    mail to: @admin_email, from: "WishlistScanner System<system@wishlistscanner.com>", subject: "WishlistScanner: Daily System Admin Email #{Date.today}"
 
   end
 
